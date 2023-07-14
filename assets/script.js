57e9e9660d8cebddce7afe53bfd2b830e91682c9
mona lisa <lizfriedman5@gmail.com>
2023-07-14 11:24:26 -0500

    read me file

--- assets/script.js
+++ assets/script.js
@@ -2,27 +2,32 @@
 var apiKey = "f8bebd04413517d30c305d0904d20aa5";
 
 
-function getWeatherByCity() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(weatherData){
-        console.log(weatherData)
-        var h2 = document.createElement('h2')
-        h2.innerText = cityName
-        var ul = document.createElement('ul')
-        var li1 = document.createElement('li')
-        var li2 = document.createElement('li')
-        var li3 = document.createElement('li')
-        li1.innerText = "Temp" + weatherData.main.temp
-        li2.innerText = "Hum" + weatherData.main.humidity
-        ul.appendChild(li1)
-        ul.appendChild(li2)
-        document.body.appendChild(h2)
-        document.body.appendChild(ul)
+// function getWeatherByCity() {
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(weatherData){
+//         console.log(weatherData)
+//         var h2 = document.createElement('h2')
+//         h2.innerText = cityName
+//         var ul = document.createElement('ul')
+//         var li1 = document.createElement('li')
+//         var li2 = document.createElement('li')
+//         var li3 = document.createElement('li')
+//         li1.innerText = "Temp" + weatherData.main.temp
+//         li2.innerText = "Hum" + weatherData.main.humidity
+//         ul.appendChild(li1)
+//         ul.appendChild(li2)
+//         document.body.appendChild(h2)
+//         document.body.appendChild(ul)
 
-    })
-}
+//     })
+// }
 
-getWeatherByCity('Milwaukee')
+// getWeatherByCity('Milwaukee')
+console.log('hello...')
+
+
+
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Weather Dashboard ⛅️
+
+## Challenge 6: Server-Side APIs
+
+For this week's challenge, I created a weather dashboard that retrieves data from a third-party API using HTML, CSS, Javascript, Bootstrap, and the Open Weather API.
+
+The purpose of this application is to allow a user/traveler to see the weather outlook for mulitple cities so that the user can plan a trip accordingly. The weather dashboard includes form inputs to search for a city and in return, gives the user a 5 day forecast of the weather. 
+
+The requirements for the assignment are listed below:
+- When I search for a city, I'm presented with current and future conditions for that city, and that city is added to the search history.
+- When I view current weather conditions for that city, I'm presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed.
+- When I view future weather conditions for that city, I'm presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+- When I click on a city in the search history, I'm presented with current and future conditions for that city.
+
+Link to deploy application: 
+
+## Mock-Up
