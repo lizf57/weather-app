4f1c1de343d5672103b6dead54569bd1644cf8a9
mona lisa <lizfriedman5@gmail.com>
2023-07-17 17:46:15 -0500

    linking API and search city function

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,67 @@
 // FOR WEATHER APP
 var apiKey = "f8bebd04413517d30c305d0904d20aa5";
 
+// search button function - current/future weather conditions for city searched and saved to the search history
 
-// function getWeatherByCity() {
-//     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey)
-//     .then(function(response){
-//         return response.json()
-//     })
-//     .then(function(weatherData){
-//         console.log(weatherData)
-//         var h2 = document.createElement('h2')
-//         h2.innerText = cityName
-//         var ul = document.createElement('ul')
-//         var li1 = document.createElement('li')
-//         var li2 = document.createElement('li')
-//         var li3 = document.createElement('li')
-//         li1.innerText = "Temp" + weatherData.main.temp
-//         li2.innerText = "Hum" + weatherData.main.humidity
-//         ul.appendChild(li1)
-//         ul.appendChild(li2)
-//         document.body.appendChild(h2)
-//         document.body.appendChild(ul)
+var searchButton = document.querySelector('.btn')
+var currentCity = document.querySelector('.currentCity')
+var currentDate = document.querySelector('#currentDate')
+var currentWeatherIcon = document.querySelector('#currentWeatherIcon')
 
-//     })
-// }
+searchButton.addEventListener('click', function (event) {
+    event.preventDefault()
 
-// getWeatherByCity('Milwaukee')
-console.log('hello...')
+    var inputValue = document.querySelector('.inputValue').value
+    getApi(inputValue)
+    let searchHistory = JSON.parse(localStorage.getItem("history")) || []
+    searchHistory.push({
+      city:inputValue
+    })
+    localStorage.setItem("history", JSON.stringify(searchHistory))
+
+  });
 
 
+
+// When I view current weather conditions for that city, I'm presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+function getApi(inputValue) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial&appid=" + apiKey)
+    .then(function(response){
+        return response.json()
+    }) 
+    .then(function(weatherInfo){
+        console.log(weatherInfo)
+        console.log(weatherInfo.name)
+        console.log(weatherInfo.coord.lat)
+        console.log(weatherInfo.coord.lon)
+        console.log(weatherInfo.main.humidity)
+        console.log(weatherInfo.main.temp)
+        console.log(weatherInfo.wind.speed)
+        currentCity.innerText = weatherInfo.name
+        
+        var todayTemp = document.querySelector("#todayTemp")
+        todayTemp.innerText=weatherInfo.main.temp + " F"
+        
+        var todayWind = document.querySelector("#todayWind")
+        todayWind.innerText = weatherInfo.wind.speed + " mph"
+
+        var todayHumid = document.querySelector("#todayHumid")
+        todayHumid.innerText = weatherInfo.main.humidity + " "
+    })
+};
+
+// var date = dayjs.format('MMM D, YYYY');
+// currentDate.innerText = date
+
+
+// let date = new Date().toJSON().split("T")[0];
+// currentDate.innerText = date
+// console.log(date)
 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+// When I view future weather conditions for that city, I'm presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+
+
+// When I click on a city in the search history, I'm presented with current and future conditions for that city.
